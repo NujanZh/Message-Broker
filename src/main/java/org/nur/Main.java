8fa5e23639040d3bbb45e90446fc582bfd9ebd75
Nurzhan <74451469+NujanZh@users.noreply.github.com>
2026-04-12 20:59:30 +0200

    Add simple multithreaded server implementation using virtual threads

--- src/main/java/org/nur/Main.java
+++ src/main/java/org/nur/Main.java
@@ -1,5 +1,35 @@
 package org.nur;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.Executors;
+
 public class Main {
+    private static final int PORT = 8080;
+
     public static void main(String[] args) {
+        try (var executor = Executors.newVirtualThreadPerTaskExecutor();
+             var serverSocket = new ServerSocket(PORT)) {
+
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                executor.submit(() -> handleClient(clientSocket));
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void handleClient(Socket socket) {
+        try (socket) {
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+            String str = in.readUTF();
+            System.out.println(Thread.currentThread().getName() + ": " + str);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
